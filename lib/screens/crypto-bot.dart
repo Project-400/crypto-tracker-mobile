8cf6205ca87e089f0cd1ea97ea53b10255da564e
Charles McCarthy <charles@studybundles.com>
2020-10-16 18:24:49 +0100

    Display Bot Events

--- lib/screens/crypto-bot.dart
+++ lib/screens/crypto-bot.dart
@@ -3,7 +3,6 @@ import 'dart:math';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:crypto_tracker/models/coin.dart';
 import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
@@ -21,6 +20,7 @@ class _CryptoBotScreenState extends State<CryptoBotScreen> {
 
   bool isBotWorking = false;
   String botError = '';
+  List<String> events = [];
 
   @override
   void initState() {
@@ -28,8 +28,6 @@ class _CryptoBotScreenState extends State<CryptoBotScreen> {
     checkBotStatus();
 
     widget.channel.stream.listen((received) {
-      print(received);
-//      Map<String, dynamic> data = jsonDecode(received);
       _sortData(received);
     });
   }
@@ -58,18 +56,85 @@ class _CryptoBotScreenState extends State<CryptoBotScreen> {
               color: Colors.lightBlue,
               textColor: Colors.white,
             ),
+            Expanded(
+              child: ListView.separated(
+                padding: EdgeInsets.all(10),
+                shrinkWrap: true,
+                scrollDirection: Axis.vertical,
+                itemCount: events.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final String event = events[index];
+
+                  return InkWell(
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.3),
+                              spreadRadius: 0.2,
+                              blurRadius: 0.5,
+                              offset: Offset(0, 0.5), // changes position of shadow
+                            ),
+                          ],
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(4),
+                              topRight: Radius.circular(4),
+                              bottomLeft: Radius.circular(4),
+                              bottomRight: Radius.circular(4)
+                          )
+                      ),
+                      child: Center(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: <Widget>[
+                            Container(
+                              child: Flexible(
+                                child: Text(
+                                  event,
+                                  style: TextStyle(
+//                                    fontWeight: FontWeight.bold,
+                                      fontSize: 14
+                                  ),
+                                ),
+                              )
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) => Divider(
+                  height: 6,
+                ),
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 
+  @override
+  void dispose() {
+    widget.channel.sink.close();
+    super.dispose();
+  }
+
   void _sortData(String msg) {
     print(msg);
+
+    setState(() {
+      events.insert(0, msg);
+      if (events.length > 50) events.removeLast();
+    });
   }
 
   Future<http.Response> checkBotStatus() async {
     final response = await http.get('http://ec2-3-250-75-62.eu-west-1.compute.amazonaws.com:3000/v1/bot-status');
+//    final response = await http.get('http://ec2-3-250-75-62.eu-west-1.compute.amazonaws.com:3000/v1/bot-status');
 
     if (response.statusCode == 200) {
       final Map<String, dynamic> data = json.decode(response.body);
