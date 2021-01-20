c1956cc66e7d9a5f2a6b9040ae722c38b0682bbc
Charles McCarthy <charles@studybundles.com>
2021-01-20 20:08:34 +0000

    Unsubscribe To Bot

--- lib/screens/subscribe.dart
+++ lib/screens/subscribe.dart
@@ -19,11 +19,13 @@ class _SubscribeScreenState extends State<SubscribeScreen> {
 
   Timer ticker;
   bool isUpdating = false;
+  String fullResponse = '';
 
   @override
   void initState() {
     super.initState();
-    setIntervalRequest();
+//    setIntervalRequest();
+//    subscribeToBot();
   }
 
   @override
@@ -37,66 +39,23 @@ class _SubscribeScreenState extends State<SubscribeScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(isUpdating ? 'Updating...' : ''),
-//            Text('Count: ' + this.widget.stats.length.toString()),
-//            Expanded(
-//              child: ListView.separated(
-//                padding: EdgeInsets.all(10),
-//                shrinkWrap: true,
-//                scrollDirection: Axis.vertical,
-//                itemCount: widget.stats != null ? widget.stats.length : 0,
-//                itemBuilder: (BuildContext context, int index) {
-//                  final PriceChangeStats stats = widget.stats[index];
-//
-//                  return InkWell(
-//                    child: Container(
-//                      padding: EdgeInsets.all(10),
-//                      decoration: BoxDecoration(
-//                          boxShadow: [
-//                            BoxShadow(
-//                              color: Colors.grey.withOpacity(0.3),
-//                              spreadRadius: 0.2,
-//                              blurRadius: 0.5,
-//                              offset: Offset(0, 0.5), // changes position of shadow
-//                            ),
-//                          ],
-//                          borderRadius: BorderRadius.only(
-//                              topLeft: Radius.circular(4),
-//                              topRight: Radius.circular(4),
-//                              bottomLeft: Radius.circular(4),
-//                              bottomRight: Radius.circular(4)
-//                          )
-//                      ),
-//                      child: Center(
-//                        child: Row(
-//                          crossAxisAlignment: CrossAxisAlignment.center,
-//                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                          children: <Widget>[
-//                            Container(
-//                              child: Text(
-//                                stats.symbol,
-//                                style: TextStyle(
-//                                    fontWeight: FontWeight.bold,
-//                                    fontSize: 16
-//                                ),
-//                              ),
-//                            ),
-//                            Text(
-//                              '${(stats.pricePercentageChanges['min5']).toStringAsFixed(2)}%',
-//                              style: TextStyle(
-//                                  fontSize: 16
-//                              ),
-//                            ),
-//                          ],
-//                        ),
-//                      ),
-//                    ),
-//                  );
-//                },
-//                separatorBuilder: (BuildContext context, int index) => Divider(
-//                  height: 6,
-//                ),
-//              ),
-//            ),
+            Text(fullResponse != null ? fullResponse : 'Waiting..'),
+            FlatButton(
+              child: Text('Start Bot'),
+              onPressed: () {
+                subscribeToBot();
+              },
+              color: Colors.lightBlue,
+              textColor: Colors.white,
+            ),
+            FlatButton(
+              child: Text('Shutdown Bot'),
+              onPressed: () {
+                unsubscribeToBot();
+              },
+              color: Colors.lightBlue,
+              textColor: Colors.white,
+            )
           ],
         ),
       ),
@@ -105,13 +64,13 @@ class _SubscribeScreenState extends State<SubscribeScreen> {
 
   @override
   void dispose() {
-    ticker.cancel();
+    if (ticker != null) ticker.cancel();
     super.dispose();
   }
 
   void setIntervalRequest() {
     subscribeToBot();
-    ticker = Timer.periodic(new Duration(seconds: 20), (timer) {
+    ticker = Timer.periodic(new Duration(seconds: 5), (timer) {
       subscribeToBot();
     });
   }
@@ -126,15 +85,44 @@ class _SubscribeScreenState extends State<SubscribeScreen> {
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
 
+      print('*********');
       print(data);
+      print('*********');
 //      final stats = (data['stats'] as List).map((s) => PriceChangeStats.fromJson(s));
 
       setState(() {
 //        widget.stats.clear();
 //        widget.stats.addAll(stats);
+      fullResponse = response.body;
       });
     } else {
       throw Exception('Failed to subscribe to Bot');
+    }
+
+    setState(() {
+      isUpdating = false;
+    });
+  }
+
+  Future<http.Response> unsubscribeToBot() async {
+    setState(() {
+      isUpdating = true;
+    });
+
+    final response = await http.get('http://localhost:3000/v1/unsubscribe');
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+
+      print('*********');
+      print(data);
+      print('*********');
+
+      setState(() {
+        fullResponse = '';
+      });
+    } else {
+      throw Exception('Failed to unsubscribe from Bot');
     }
 
     setState(() {
