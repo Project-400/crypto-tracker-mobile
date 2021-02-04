400cda186d6c025c6a836fa10657da45f9feeeae
Charles McCarthy <charles@studybundles.com>
2021-02-04 23:10:49 +0000

    Refactored Home Buttons & Added BTC SVG Image

--- lib/constants/screen-titles.dart
+++ lib/constants/screen-titles.dart
@@ -1,10 +1,10 @@
 class ScreenTitles {
 
-  static const String HOME_SCREEN = 'Crypto Tracker';
+  static const String HOME_SCREEN = 'Crypto Bot';
   static const String COINS_SCREEN = 'My Assets';
-  static const String BOT_SCREEN = 'Crypto Bot';
+//  static const String BOT_SCREEN = 'Crypto Bot';
   static const String BEST_PERFORMERS = 'Best Performers';
-  static const String SUBSCRIBE = 'Subscribe';
+  static const String SUBSCRIBE = 'Deploy Bot';
   static const String BOT_FINISHED = 'Bot Finished';
 
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:crypto_tracker/screens/bot-finished.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key, this.title}) : super(key: key);
@@ -21,49 +22,65 @@ class _HomeScreenState extends State<HomeScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FlatButton(
-              child: Text('My Assets'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/coins');
-              },
-              color: Colors.lightBlue,
-              textColor: Colors.white,
+            Container(
+              child: SvgPicture.asset(
+                'assets/bitcoin.svg',
+                semanticsLabel: 'Preparing Bot',
+                placeholderBuilder: (BuildContext context) => Container(
+                    padding: const EdgeInsets.all(30.0),
+                    child: const CircularProgressIndicator()
+                ),
+                height: 300,
+              ),
+              padding: EdgeInsets.symmetric(vertical: 80, horizontal: 10),
             ),
-//            FlatButton(
-//              child: Text('Crypto Bot'),
-//              onPressed: () {
-//                Navigator.pushNamed(context, '/bot');
-//              },
-//              color: Colors.lightBlue,
-//              textColor: Colors.white,
-//            ),
-            FlatButton(
-              child: Text('Best Performers'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/best-performers');
-              },
-              color: Colors.lightBlue,
-              textColor: Colors.white,
-            ),
-            FlatButton(
-              child: Text('Subscribe'),
-              onPressed: () {
-                Navigator.pushNamed(context, '/subscribe');
-              },
-              color: Colors.lightBlue,
-              textColor: Colors.white,
-            ),
-            FlatButton(
-              child: Text('Skip to Finished'),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => BotFinishedScreen(title: 'Bot Finished', botId: '32b6ed51-b267-4d22-84c7-9b794028c21b')));
-              },
-              color: Colors.lightBlue,
-              textColor: Colors.white,
-            ),
+            navigationButton(context, 'My Assets', '/coins'),
+            navigationButton(context, 'Best Performers', '/best-performers'),
+            navigationButton(context, 'Deploy Bot', '/subscribe'),
+            navigationButtonWithScreen(context, 'Skip to Finished', MaterialPageRoute(builder: (context) => BotFinishedScreen(title: 'Bot Finished', botId: '32b6ed51-b267-4d22-84c7-9b794028c21b'))),
           ],
         ),
       ),
+    );
+  }
+
+  Widget navigationButton(BuildContext context, String text, String url) {
+    return Container(
+      child: RaisedButton(
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 16
+          ),
+        ),
+        onPressed: () {
+          Navigator.pushNamed(context, url);
+        },
+        color: Colors.lightBlue,
+        textColor: Colors.white,
+      ),
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
+    );
+  }
+
+  Widget navigationButtonWithScreen(BuildContext context, String text, MaterialPageRoute screenRoute) {
+    return Container(
+      child: RaisedButton(
+        child: Text(
+          text,
+          style: TextStyle(
+              fontSize: 16
+          ),
+        ),
+        onPressed: () {
+          Navigator.push(context, screenRoute);
+        },
+        color: Colors.lightBlue,
+        textColor: Colors.white,
+      ),
+      width: double.infinity,
+      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 50),
     );
   }
 }
