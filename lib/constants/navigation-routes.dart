327e0209cd2f6d5a45c8f959c14f9c46bda28707
Charles McCarthy <charles@studybundles.com>
2021-03-07 19:33:19 +0000

    Navigation Routes Enum

---
+++ lib/constants/navigation-routes.dart
@@ -0,0 +1,10 @@
+class NavigationRoutes {
+
+  static const String HOME_SCREEN = '/';
+  static const String COINS_SCREEN = '/coins';
+  static const String BEST_PERFORMERS = '/best-performers';
+  static const String SUBSCRIBE = '/subscribe';
+  static const String BOT_FINISHED = '/bot-finished';
+  static const String PRICE_CHARTS = '/price-charts';
+
+}
