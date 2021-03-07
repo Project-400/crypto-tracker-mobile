6db0ab0647e86a68415e246a6946de4f4101e6c9
Charles McCarthy <charles@studybundles.com>
2021-03-07 19:34:09 +0000

    Resuable Bottom Navigation Bar

---
+++ lib/components/bottom-navigation.dart
@@ -0,0 +1,64 @@
+import 'package:crypto_tracker/constants/navigation-routes.dart';
+import 'package:crypto_tracker/constants/screen-titles.dart';
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatelessWidget {
+  BottomNavBar({Key key, this.selectedScreen}) : super(key: key);
+
+  final String selectedScreen;
+
+  @override
+  Widget build(BuildContext context) {
+    num currentIndex = 0;
+
+    switch(selectedScreen) {
+      case ScreenTitles.HOME_SCREEN:
+        currentIndex = 0;
+        break;
+      case ScreenTitles.COINS_SCREEN:
+        currentIndex = 1;
+        break;
+      case ScreenTitles.PRICE_CHARTS:
+        currentIndex = 2;
+        break;
+    }
+
+    return BottomNavigationBar(
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          label: ScreenTitles.HOME_SCREEN,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.attach_money),
+          label: ScreenTitles.COINS_SCREEN,
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.show_chart_sharp),
+          label: ScreenTitles.PRICE_CHARTS,
+        ),
+//              BottomNavigationBarItem(
+//                icon: Icon(Icons.queue_outlined),
+//                label: 'Bots',
+//              ),
+      ],
+      currentIndex: currentIndex,
+      selectedItemColor: Colors.amber[800],
+      onTap: (index) {
+        String url = '/';
+        switch (index) {
+          case 0:
+            url = NavigationRoutes.HOME_SCREEN;
+            break;
+          case 1:
+            url = NavigationRoutes.COINS_SCREEN;
+            break;
+          case 2:
+            url = NavigationRoutes.PRICE_CHARTS;
+            break;
+        }
+        Navigator.pushNamed(context, url);
+      },
+    );
+  }
+}
--- lib/constants/screen-titles.dart
+++ lib/constants/screen-titles.dart
@@ -1,6 +1,6 @@
 class ScreenTitles {
 
-  static const String HOME_SCREEN = 'Crypto Bot';
+  static const String HOME_SCREEN = 'Home';
   static const String COINS_SCREEN = 'My Assets';
 //  static const String BOT_SCREEN = 'Crypto Bot';
   static const String BEST_PERFORMERS = 'Best Performers';
