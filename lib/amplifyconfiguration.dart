e8ed03802ebbdad188348b4fdeb69cf04c8b9cbc
Charles McCarthy <charles@studybundles.com>
2021-04-05 20:19:11 +0100

    Updated Amplify Config

--- lib/amplifyconfiguration.dart
+++ lib/amplifyconfiguration.dart
@@ -1,4 +1,36 @@
 const amplifyconfig = ''' {
     "UserAgent": "aws-amplify-cli/2.0",
-    "Version": "1.0"
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "eu-west-1:4a7b0578-7dd1-4b02-8b89-302c58198e8c",
+                            "Region": "eu-west-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "eu-west-1_N5kVW2QF6",
+                        "AppClientId": "3hfaicgf4cc3ckcnb2d7epp335",
+                        "AppClientSecret": "110v16nj4erjib6k0536rv5qkdm0hukig7aqq920mrchvgpso7jb",
+                        "Region": "eu-west-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
 }''';
