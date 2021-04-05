430f54abb4bc96943e8bca6c0af20cff9a828c5e
Charles McCarthy <charles@studybundles.com>
2021-04-05 21:57:29 +0100

    Sign Up Screen With Cognito Call

---
+++ lib/screens/sign-up.dart
@@ -0,0 +1,102 @@
+import 'dart:async';
+
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class SignUpScreen extends StatefulWidget {
+  SignUpScreen({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _SignUpScreenState createState() => _SignUpScreenState();
+}
+
+class _SignUpScreenState extends State<SignUpScreen> {
+
+  bool isSigningUp = false;
+  String emailAddress;
+  String password;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: TextField(
+                decoration: InputDecoration(
+                    labelText: 'Email Address',
+                    hintText: 'Email Address'
+                ),
+                onChanged: (email) => emailAddress = email,
+              ),
+              padding: EdgeInsets.all(10),
+            ),
+            Container(
+              child: TextField(
+                obscureText: true,
+                enableSuggestions: false,
+                autocorrect: false,
+                decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Password'
+                ),
+                onChanged: (pw) => password = pw,
+              ),
+              padding: EdgeInsets.all(10),
+            ),
+            ElevatedButton(
+              child: Text('Sign Up'),
+              onPressed: signUp
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Future<void> signUp() async {
+    setState(() {
+      isSigningUp = true;
+    });
+
+    try {
+      SignUpResult res = await Amplify.Auth.signUp(
+          username: emailAddress,
+          password: password,
+          options: CognitoSignUpOptions(
+              userAttributes: {
+                'email': emailAddress
+              }
+          )
+      );
+
+      print(res);
+    } on AuthException catch (e) {
+      print(e.message);
+    }
+
+    setState(() {
+      isSigningUp = false;
+    });
+  }
+
+}
