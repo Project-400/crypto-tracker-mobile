266b3496293163fb3115c99f342363a2d430e8c0
Charles McCarthy <charles@studybundles.com>
2021-04-05 20:19:30 +0100

    Changed App Main Widget To Stateful

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,51 @@ import 'package:redux/redux.dart';
 import 'constants/screen-titles.dart';
 import 'navigation.dart';
 
+import 'package:amplify_flutter/amplify.dart';
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'amplifyconfiguration.dart';
+
 void main() {
   runApp(CryptoTrackerApp(store: store));
 }
 
-class CryptoTrackerApp extends StatelessWidget {
+class CryptoTrackerApp extends StatefulWidget {
   final Store<CoinsState> store;
 
   CryptoTrackerApp({ this.store });
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<CryptoTrackerApp> {
+  bool _amplifyConfigured = false;
+
+  @override
+  State<StatefulWidget> initState() {
+    super.initState();
+    _configureAmplify();
+  }
+
+  void _configureAmplify() async {
+    if (mounted) return; // Avoid reconfiguring Amplify or error will occur
+
+    Amplify.addPlugin(AmplifyAuthCognito());
+
+    try {
+      await Amplify.configure(amplifyconfig);
+      print('Amplify successfully configured');
+    } on AmplifyAlreadyConfiguredException {
+      print('Amplify was already configured. Was the app restarted?');
+    }
+    try {
+      this.setState(() {
+        _amplifyConfigured = true;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,4 +67,5 @@ class CryptoTrackerApp extends StatelessWidget {
       ),
     );
   }
+
 }
